eb19ef3702dfcb77ccb9ab4dbe4dd67e5d3c6113
nalinShankar <nalinmakaut@gmail.com>
2021-04-04 17:30:06 +0530

    project structure initialization

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path:'',
+        component: HomeComponent
+      },
+      {
+        path:'login',
+        component: LoginComponent
+      },
+      {
+        path:'admin',
+        component: AdminComponent
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
